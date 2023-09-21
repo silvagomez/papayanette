6779c9888ccba7819ecdbee611ec8ff987ff0ae8
Diego Camilo Silva Gomez <dsilva-g@c4r1s4.42urduliz.com>
2023-09-21 19:16:21 +0200

    create: stub functions

---
+++ 42tester-libft/ft_stubs.c
@@ -0,0 +1,163 @@
+#include "papaya_lib.h"
+
+int		ft_isalpha(int c)
+{
+	return (0);
+}
+int		ft_isdigit(int c)
+{
+	return (0);
+}
+int		ft_isalnum(int c)
+{
+	return (0);
+}
+int		ft_isascii(int c)
+{
+	return (0);
+}
+int		ft_isprint(int c)
+{
+	return (0);
+}
+size_t	ft_strlen(const char *s);
+{
+	return (0);
+}
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+{
+	return (0);
+}
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+{
+	return (0);
+}
+char	*ft_strchr(const char *s, int c);
+{
+	return (NULL);
+}
+char	*ft_strrchr(const char *s, int c);
+{
+	return (NULL);
+}
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	return (0);
+}
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	return (NULL);
+}
+void	*ft_memset(void *b, int c, size_t len)
+{
+	return (NULL);
+}
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	return (NULL);
+}
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	return (NULL);
+}
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	return (0);
+}
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	return (NULL);
+}
+void	ft_bzero(void *s, size_t n)
+{
+}
+int		ft_toupper(int c)
+{
+	return (0);
+}
+int		ft_tolower(int c)
+{
+	return (0);
+}
+int		ft_atoi(const char *str)
+{
+	return (0);
+}
+void	*ft_calloc(size_t count, size_t size)
+{
+	return (NULL);
+}
+char	*ft_strdup(const char *s1)
+{
+	return (NULL);
+}
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	return (NULL);
+}
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	return (NULL);
+}
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	return (NULL);
+}
+char	**ft_split(char const *s, char c)
+{
+	return (NULL);
+}
+char	*ft_itoa(int n)
+{
+	return (NULL);
+}	
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	return (NULL);
+}
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+}
+void	ft_putchar_fd(char c, int fd)
+{
+}
+void	ft_putstr_fd(char *s, int fd)
+{
+}
+void	ft_putendl_fd(char *s, int fd)
+{
+}
+void	ft_putnbr_fd(int n, int fd)
+{
+}
+t_list	*ft_lstnew(void *content)
+{
+	return (NULL);
+}
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+}
+int		ft_lstsize(t_list *lst)
+{
+	return (0);
+}
+t_list	*ft_lstlast(t_list *lst)
+{
+	return (NULL);
+}
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+}
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+}
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+}
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+}
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	return (NULL);
+}
